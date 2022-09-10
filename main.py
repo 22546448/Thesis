f111e552321d696a0416ae5f34f8f6bc138cbca6
Christiaan <22546448@sun.ac.za>
2022-09-10 12:42:42 +0200

    Added  sympy and made methods class dependend

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from test import GetField, PowerAtPoint, getSdata, plot2D, plotPowerLine,plot2DColor
+from EMF import GetField
 import numpy as np
 from matplotlib import pyplot as plt
 import pandas as pd
@@ -9,13 +9,14 @@ import pandas as pd
 surface1 = GetField("IEC-62232-panel-antenna_2Dxy.efe","IEC-62232-panel-antenna_2Dxy.hfe")
 df = surface1.dataframe
 
-S = getSdata(df)
+#S = surface1.getSdata(df)
 
 #plot2DColor(S,'X','Y')
 
 #plot2D(df,'X','Y','|E|')
 #plot2D(df,'X','Y','|H|')
-plot2D(df,'X','Y','S(E)')
+print(df)
+surface1.plot2D(df,'X','Y','Im(Ex)')
 
 
 
--- temp.py
+++ temp.py
@@ -1,38 +1,10 @@
-import numpy as np
-from matplotlib import pyplot as plt
-import matplotlib.cm as cm
-import pandas as pd
+from sympy.physics.vector import ReferenceFrame
+from sympy.physics.vector import curl
+R = ReferenceFrame('R')
+
+F = R[1]**2 * R[2] * R.x - R[0]*R[1] * R.y + R[2]**2 * R.z
+
+G = curl(F, R) 
 
 
-
-
-
-# Program to draw scatter plot using Dataframe.plot
-# Import libraries
-import pandas as pd
-
-# Prepare data
-import matplotlib.pyplot as plt
-import matplotlib.cm as cm
-import matplotlib.colors as colors
-import numpy as np
-import pandas as pd
-
-fig, ax = plt.subplots()
-
-df = pd.DataFrame({'n1':[1,2,1,3], 'n2':[1,3,2,1], 'l':['a','b','c','d']})
-
-colormap = cm.viridis
-colorlist = [colors.rgb2hex(colormap(i)) for i in np.linspace(0, 0.9, len(df['l']))]
-print(colorlist)
-for i,c in enumerate(colorlist):
-
-    x = df['n1'][i]
-    y = df['n2'][i]
-    l = df['l'][i]
-
-    ax.scatter(x, y, label=l, s=50, linewidth=0.1, c=c)
-
-ax.legend()
-
-plt.show()
+print(F)
