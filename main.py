915776d8b9db95def2a0785a13f97d6b12ef1500
Christiaan <22546448@sun.ac.za>
2022-09-13 17:21:53 +0200

    added ixus zone plotting

--- main.py
+++ main.py
@@ -1,24 +1,33 @@
 from EMFFeko import GetField
+import EMFIXUS
 from CreateEMF import Surface,Antenna,Field
 from matplotlib import pyplot as plt
 import pandas as pd
+import numpy as np
+
+from EMFIXUS import IXUSField
+
+#surface1 = GetField("IEC-62232-panel-antenna (4)_NearField1.efe","IEC-62232-panel-antenna (4)_NearField1.hfe")
+#df['R'] = np.sqrt(df['X']**2 + df['Y']**2 + df['Z']**2)
+#df.loc[df['% of ICNIRP Public'] > 4, '% of ICNIRP Public'] = 0.1
 
 
-surface1 = GetField("IEC-62232-panel-antenna_2Dxy.efe","IEC-62232-panel-antenna_2Dxy.hfe")
-df = pd.read_csv('IXUS/EnvironmentalSlice1.csv')
 
 
-surface1.plot2DZones()
+surface3 = IXUSField('EnvironmentalSlice2-1.csv',900)
+surface3.plot2DZones()
 
-plane1 = Surface(-5,100,5,-5,100,5)
-antenna1 = Antenna(f=900,P=50,G=30)
-surface2 = Field(antenna1,plane1)
+
+#surface1.plot2DZones()
+
+#plane1 = Surface(-50,100,50,-50,100,50)
+#antenna1 = Antenna(f=900,P=50,G=30,y = 2,z=-5)
+#surface2 = Field(antenna1,plane1)
 #surface2.plot2DZones(show = True)
 
 
-
-#plt.scatter(x= df['X'],y=df['Y'],c=df['% of ICNIRP Public'],cmap='Reds')
-
+#ax = plt.scatter(x= df['X'],y=df['Y'],c=df['% of ICNIRP Public'],cmap='Reds')
+#plt.colorbar(ax)
 
 #plt.scatter(x=surface2['X'],y=surface2['Y'],c=surface2['S'],cmap='Reds')
-plt.show()
+#plt.show()
