8b19b55d3c3622d4020d0c1bf4719b8792693186
Christiaan <22546448@sun.ac.za>
2022-09-29 19:50:00 +0200

    Cylindrical Validation

--- EMFFeko.py
+++ EMFFeko.py
@@ -1,3 +1,4 @@
+from cmath import sin
 from fileinput import filename
 from platform import freedesktop_os_release
 from unittest import result
@@ -18,7 +19,7 @@ warnings.filterwarnings('ignore', category=NaturalNameWarning)
 
 
 class Fekofield(Field):
-    def __init__(self,source,frequency,coordSystem, xSamples, ySamples, zSamples,standard,df):
+    def __init__(self,power,source,frequency,coordSystem, xSamples, ySamples, zSamples,standard,df):
         super().__init__(df,frequency*(10**-6),type = 'Feko',standard=standard, restriction = False)
         self.source = source
         self.frequency = frequency
@@ -26,6 +27,7 @@ class Fekofield(Field):
         self.xSamples = xSamples
         self.ySamples = ySamples
         self.zSamples = zSamples
+        self.power = power
 
         i = 0
         if xSamples > 1: i+=1
@@ -58,10 +60,46 @@ class Fekofield(Field):
             mlab.show()
 
 
-                
-def GetField(filenameE,filenameH,S = 'S(E)',compress = True,standard = 'FCC'):
+def GetFarField(filename,compress = True,standard = 'FCC',power = 80):
     source= ''
-    frequency= 0
+    frequency= 900
+    coordSystem= ''
+    thetaSamples = 0
+    phiSamples = 0
+    global i
+    i = 0
+    filenameff = 'venv/Include/CADFeko/{}'.format(filename)
+
+    with open(filenameff, 'r') as file:
+        for line in file:
+            if '##Source: ' in line:
+                source = line[:-1].split("##Source: ",1)[1]
+            elif "#Frequency: " in line:
+                frequency = int(float(line[:-1].split("#Frequency:   ",1)[1]))  
+            elif "#Coordinate System: " in line:
+                coordSystem = line[:-1].split("#Coordinate System: ",1)[1]
+            elif "#No. of Theta Samples: " in line:
+                thetaSamples = int(line[:-1].split("#No. of Theta Samples: ",1)[1])
+            elif "#No. of Phi Samples: " in line:
+                phiSamples = int(line[:-1].split("#No. of Phi Samples: ",1)[1])
+                global dataT
+                dataT = np.zeros((thetaSamples*phiSamples,9)) 
+            if line[0] != '#' and line[0] != '*' and line[0] != '\n':
+                dataT[i] = line[4:-1].split('   ')
+                i+=1
+        df = pd.DataFrame(dataT,columns=['Theta','Phi','Re(Etheta)','Im(Etheta)','Re(Ephi)','Im(Ephi)','Directivity(Theta)','Directivity(Phi)','Directivity(Total)'])
+        df = df.astype(float)
+    file.close()
+
+    df['Etheta'] = (df['Re(Etheta)'] + df['Im(Etheta)']*1j)/np.sqrt(2)
+    df['Ephi'] = (df['Re(Ephi)'] + df['Im(Ephi)']*1j)/np.sqrt(2)
+    df['|E|'] = np.sqrt(np.absolute(df['Etheta'])**2 + np.absolute(df['Ephi'])**2)
+    df['S(E)'] = df['|E|']**2/(337*2)
+    return df
+
+def GetField(filenameE,filenameH,S = 'S(E)',compress = True,standard = 'FCC',power = 80):
+    source= ''
+    frequency= 900
     coordSystem= ''
     xSamples= 0
     ySamples= 0
@@ -93,9 +131,9 @@ def GetField(filenameE,filenameH,S = 'S(E)',compress = True,standard = 'FCC'):
                 i+=1
         df = pd.DataFrame(dataT,columns=['X','Y','Z','Re(Ex)','Im(Ex)','Re(Ey)','Im(Ey)','Re(Ez)','Im(Ez)'])
         df = df.astype(float)
-        #df['R'] = np.sqrt(df['X']**2 + df['Y']**2 + df['Z']**2)
-        #df['phi'] = np.arccos(df['Z']/df['R'])
-        #df['theta'] = np.arccos(df['X']/(df['R']*np.sin(df['phi'])))
+        df['R'] = np.sqrt(df['X']**2 + df['Y']**2 + df['Z']**2)
+        df['phi'] = np.arctan(df['Y']/df['X'])
+        df['theta'] = np.arcsin(df['X']/df['R'])
     file.close()
 
     with open(filenameH, 'r') as file:
@@ -114,25 +152,22 @@ def GetField(filenameE,filenameH,S = 'S(E)',compress = True,standard = 'FCC'):
     df['Im(Hz)'] = dataH[:,5]
 
     df['Ex'] = (df['Re(Ex)'] + df['Im(Ex)']*1j)/np.sqrt(2)
+    df['|Ex|'] = np.absolute(df['Ex'])
     df['Ey'] = (df['Re(Ey)'] + df['Im(Ey)']*1j)/np.sqrt(2)
+    df['|Ey|'] = np.absolute(df['Ey'])
     df['Ez'] = (df['Re(Ez)'] + df['Im(Ez)']*1j)/np.sqrt(2)
+    df['|Ez|'] = np.absolute(df['Ez'])
     df['Hx'] = (df['Re(Hx)'] + df['Im(Hx)']*1j)/np.sqrt(2)
     df['Hy'] = (df['Re(Hy)'] + df['Im(Hy)']*1j)/np.sqrt(2)
     df['Hz'] = (df['Re(Hz)'] + df['Im(Hz)']*1j)/np.sqrt(2)
 
- 
-    
-    #df['|E|'] = np.absolute(df['Ez'])
     df['|E|'] = np.sqrt(np.absolute(df['Ex'])**2+ np.absolute(df['Ey'])**2 + np.absolute(df['Ez'])**2)
-    
-
-    #df['|H|'] = np.sqrt(df['Re(Hx)']**2  + df['Im(Hx)']**2 + df['Re(Hy)']**2  + df['Im(Hy)']**2  + df['Re(Hz)']**2 + df['Im(Hz)']**2)
-
     df['S(E)'] = df['|E|']**2/(377)
-    #df['S(E2)'] = (df['|E|']**2)/(2*377)
-    #df['S(R)'] = (df['|E|']**2)/(377)
+    #df['Snear'] = SpacialPeakCylindricalEstimation(df['phi'], df['R'])
+    df['Sfar'] = AdjustedSphericalSector(theta=df['theta'] , phi=df['phi'] , R=df['R'])
+    df['Snear'] = SpacialPeakCylindricalEstimation(df['phi'],df['R'])
+    df['SfarSimple'] = SimpleSphericalSector(theta=df['theta'] , phi=df['phi'] , R=df['R'])
 
-    #P = 
 
     df['Sx'] = df['Ey']*df['Hz'] - df['Ez']*df['Hy']
     df['Sy'] = df['Ez']*df['Hx'] - df['Ex']*df['Hz']
@@ -140,22 +175,180 @@ def GetField(filenameE,filenameH,S = 'S(E)',compress = True,standard = 'FCC'):
     df['S(ExH)'] = np.sqrt(np.absolute(df['Sx'])**2 + np.absolute(df['Sy'])**2 + np.absolute(df['Sz'])**2)
 
 
-    df['Sx*'] = np.real(df['Ey']*np.conj(df['Hz']) - df['Ez']*np.conj(df['Hy']))
-    df['Sy*'] = np.real(df['Ez']*np.conj(df['Hx']) - df['Ex']*np.conj(df['Hz']))
-    df['Sz*'] = np.real(df['Ex']*np.conj(df['Hy']) - df['Ey']*np.conj(df['Hx']))
-    df['S(ExH*)'] = np.sqrt(df['Sx*']**2 + df['Sy*']**2 + df['Sz*']**2)/2
+    #df['Sx*'] = np.real(df['Ey']*np.conj(df['Hz']) - df['Ez']*np.conj(df['Hy']))
+    #df['Sy*'] = np.real(df['Ez']*np.conj(df['Hx']) - df['Ex']*np.conj(df['Hz']))
+    #df['Sz*'] = np.real(df['Ex']*np.conj(df['Hy']) - df['Ey']*np.conj(df['Hx']))
+    #df['S(ExH*)'] = np.sqrt(df['Sx*']**2 + df['Sy*']**2 + df['Sz*']**2)/2
+
     df['S'] = df['S(ExH)']
     if compress:
         df = df.drop(columns = ['Re(Ex)','Im(Ex)','Re(Ey)','Im(Ey)','Re(Ez)','Im(Ez)','Re(Hx)','Im(Hx)','Re(Hy)','Im(Hy)','Re(Hz)','Im(Hz)','Re(Sx)','Im(Sx)','Re(Sy)','Im(Sy)','Re(Sz)','Im(Sz)','Ex','Ey','Ez','Hx','Hy','Hz','|E|'])
     
-
-    #hdf = HDFStore('hdf_file.h5')
-    #hdf.put('EMF', df, format='table', data_columns=True) #put data in hdf file
-    #hdf.close()
-    return Fekofield(source,frequency,coordSystem, xSamples, ySamples, zSamples,standard,df)
+    return Fekofield(source,power,frequency,coordSystem, xSamples, ySamples, zSamples,standard,df)
 
 
+# all Sector coverage arrays
+def SpacialAverageCylindricalEstimation(phi,R,P = 80,AHPBW = 85,L = 2.25,G= 17,y = 0,ry = None):
+    G = 10**(G/10)
+    AHPBW = np.pi*AHPBW/180
+    ro = AHPBW*G*L*np.cos(y)**2/12
+
+    
+    ry = R/np.cos(y)
+    return P*2**(-1*(2*phi/AHPBW)**2)/(AHPBW*ry*L*(np.cos(y)**2)*np.sqrt(1 + (ry/ro)**2))
+
+def SpacialPeakCylindricalEstimation(phi,R,P = 80,AHPBW = 85,L = 2.25,G= 17,y = 0):
+    G = 10**(G/10)
+    AHPBW *= np.pi/180
+    ro = AHPBW*G*L*np.cos(y)**2/12
+    ry = 0
+    ry = R/np.cos(y)
+    return 2*P*2**(-4*(phi/AHPBW)**2)/(AHPBW*ry*L*np.cos(y)**2*np.sqrt(1 + (2*ry/ro)**2))
+
+def AdjustedSphericalSector(theta,phi,R,power = 80, VHPBW = 8.5, AHPBW = 85, L = 2.25, G = 17,Globe = 3.6, y = 0):
+    G = 10**(G/10)
+    VHPBW *= np.pi/180
+    AHPBW *= np.pi/180
+    b1 = (theta - y - np.pi/2)/VHPBW
+    b2 = 1.9*phi/AHPBW
+    Gphitheta = 1.26*Globe + G*2**(-b1**2-b2**2)
+    return 1.2*power*Gphitheta/(4*np.pi*R**2)
+
+def SimpleSphericalSector(theta,phi,R,power = 80, VHPBW = 8.5, AHPBW = 85, L = 2.25, G = 17,Globe = 0, y = 0):
+    G = 10**(G/10)
+    VHPBW *= np.pi/180
+    AHPBW *= np.pi/180
+    b1 = (2*(theta - y - np.pi/2)/VHPBW)**2
+    b2 = (2*phi/AHPBW)**2
+    Gphitheta = Globe + G*2**(-b1-b2)
+    return power*Gphitheta/(4*np.pi*R**2)
+
+
+
+def ClassicalSpherical(df,power = 80, L = 2.25):
+    return df['|E|']**2/(377)
+
+#def plotValidation1(*method):
+#    for meth in method:
+
+def nearSurface(ff,P = 80, h = 2.25, AHPBW = 8.5,dipoles = 9):
+    x = 0.1
+    linegain = ff.loc[ff['Phi'] == 0]
+    Z = np.linspace(-1.4,1.4,15)
+    R = [np.sqrt(z**2 + x**2) for z in Z]
+    angle = [np.round(np.abs(np.arcsin(x/r)*180/np.pi)) for r in R]
+    D = []
+    for a in angle:
+        D.append(linegain.loc[linegain['Theta'] == a,'Directivity(Total)'].to_numpy()[0])
+    #S0 = [180*P/(dipoles*AHPBW*2*np.pi*np.sqrt(x**2 + (z+1.25)**2)*h) for z in Z]
+    S1 = [180*P*(dipoles*AHPBW*2*np.pi*np.sqrt(x**2 + (z+1)**2)*h) for z in Z]
+    S2 = [180*P/(dipoles*AHPBW*2*np.pi*np.sqrt(x**2 + (z+0.75)**2)*h) for z in Z]
+    S3 = [180*P/(dipoles*AHPBW*2*np.pi*np.sqrt(x**2 + (z+0.5)**2)*h) for z in Z]
+    S4 = [180*P/(dipoles*AHPBW*2*np.pi*np.sqrt(x**2 + (z+0.25)**2)*h) for z in Z]
+    S5 = [180*P/(dipoles*AHPBW*2*np.pi*np.sqrt(x**2 + (z)**2)*h) for z in Z]
+    S6 = [180*P/(dipoles*AHPBW*2*np.pi*np.sqrt(x**2 + (z-0.25)**2)*h) for z in Z]
+    S7 = [180*P/(dipoles*AHPBW*2*np.pi*np.sqrt(x**2 + (z-0.5)**2)*h) for z in Z]
+    S8 = [180*P/(dipoles*AHPBW*2*np.pi*np.sqrt(x**2 + (z-0.75)**2)*h) for z in Z]
+    S9 = [180*P/(dipoles*AHPBW*2*np.pi*np.sqrt(x**2 + (z-1)**2)*h) for z in Z]
+    #S10 =[180*P/(dipoles*AHPBW*2*np.pi*np.sqrt(x**2 + (z-1.25)**2)*h) for z in Z]
+    temp = []
+    for i in range(len(Z)):
+        temp.append( (D[i]+2)*(S1[i] +  S2[i] + S3[i] + S4[i] + S5[i] + S6[i] + S7[i] + S8[i] + S9[i]))
+    return D
+    
 
 #def CreateField(antenna,source='IEC-62232-panel-antenna',date=0,configuration='Created',frequency = f)
 #   return fieldSolved(name,Fileformat,source,date,solverV,configuration,frequency,coordSystem, xSamples, ySamples, zSamples,df)
+
+def test1():
+    ff = GetFarField('test1.ffe')
+    f = ff.loc[(ff['Directivity(Total)'] > 11.96) & (ff['Directivity(Total)'] < 12.1) & (ff['Phi'] < 181) & (ff['Theta'] == 90)]
+    print(f.sort_values(by = ['Directivity(Total)']))
+
+    line3near = nearSurface(ff = ff)
+        #line1
+    def ffline1():
+        line1 = 10**(ff.loc[(ff['Phi'] == 0) & (ff['Theta'] == 90),'Directivity(Total)'].to_numpy()[0]/10)
+        #line1 = (ff.loc[(ff['Phi'] == 0) & (ff['Theta'] == 90),'Directivity(Total)'].to_numpy()[0])
+        R = np.linspace(0.5,4,8)
+        return [80*line1/(4*np.pi*r**2) for r in R]
+    
+    #line2
+    def ffline2(x = 1):
+        line2 = ff.loc[(ff['Phi'] < 181) & (ff['Theta'] == 90)]
+        Y = np.linspace(-1.4,1.4,15)
+        R = [np.sqrt(y**2 + x**2) for y in Y]
+        angle = [np.abs(np.round(np.arccos(x/r)*180/np.pi)) for r in R]
+        D = []
+        for a in angle:
+            D.append(10**(line2.loc[line2['Phi'] == a,'Directivity(Total)'].to_numpy()[0]/10))
+
+#            D.append(line2.loc[line2['Phi'] == a,'Directivity(Total)'].to_numpy()[0])
+
+        #num = [10**(d/10) for d in D]
+        #lamda = 1/3
+        #D = [20*np.log10(9.73/(lamda*np.sqrt(n))) for n in num]
+
+        return [80*d/(4*np.pi*r**2) for d,r in zip(D,R) ]
+
+
+
+    def ffline3(x = 0.1):
+        linegain = ff.loc[ff['Phi'] == 0]
+        Z = np.linspace(-1.4,1.4,15)
+        R = [np.sqrt(z**2 + x**2) for z in Z]
+        angle = [np.round(np.abs(np.arcsin(x/r)*180/np.pi)) for r in R]
+        D = []
+        for a in angle:
+            D.append(10**(linegain.loc[linegain['Theta'] == a,'Directivity(Total)'].to_numpy()[0]/10))
+        #num = [10**(d/10) for d in D]
+        #lamda = 1/3
+        #D = [20*np.log10(9.73/(lamda*np.sqrt(n))) for n in num]
+        return [80*d/(4*np.pi*r**2) for d,r in zip(D,R) ]
+        
+
+
+    FCCoccupational = 30#getZone(900,'FCC')[1]
+    l1 = [65.8, 39.4, 25, 13.4, 10.7, 10.4, 10.1, 9.44]
+    l2 = [6.22, 9.16, 13.6, 19.6, 26.3, 32.8, 37.6, 39.4, 37.6, 32.8, 26.3, 19.6, 13.6, 9.16, 6.22]
+    l3 = [0.173, 3.77, 129, 105, 111, 163, 272, 338, 272, 163, 111, 105, 129, 3.77, 0.173]
+
+    IXUS1_persentage_occupation = [243.8, 138.7, 84.43, 43.62, 36.72, 36.46, 35.51, 33.07]
+    IXUS2_persentage_occupation = [19.2, 29.18, 44.46, 66.19, 91.01, 114.7, 133, 138.7, 132.1, 114.1, 90.44, 65.52, 44.46, 29.18, 19.19]
+    IXUS3_persentage_occupation = [3.623, 24.51, 595, 273.1, 508.7, 682.9, 869.8, 1279, 886.2, 683.6, 505.8, 267.4, 594.2, 24.24, 3.706]
+
+
+    def Doline(*lines):
+        
+        for line in lines:
+            plt.figure()
+            plt.plot(line['1D'], line['l'],label = 'Validation line')
+            plt.plot(line['1D'], line['IXUS'],label = 'IXUS')
+            plt.plot(line['1D'], line['S(E)'], label = '|E|^2/377')
+            plt.plot(line['1D'],line['Sff'],label = 'Sff')
+            #plt.plot(line['1D'], line['S(ExH)'], label = 'S=ExH')
+            plt.plot(line['1D'], line['Snear'], label = 'Snear')
+            #plt.plot(line['1D'], line['Sfar'], label = 'Sfar')
+            #plt.plot(np.linspace(-1.4,1.4,15),line3near,label = 'nearSurface')
+
+            #axs[0, 0].plot(line1.df['X'], line1.df['S(R)far'], label = 'S(R)far')
+            plt.legend()
+
+
+    line1 = GetField('IEC-62232-panel-antenna (4)_Line1.efe','IEC-62232-panel-antenna (4)_Line1.hfe',compress=False, power=80).df
+    line1['IXUS'] = [x/100*FCCoccupational for x in IXUS1_persentage_occupation]
+    line1['Sff'] = ffline1()
+    line1['l'] = l1
+    line2 = GetField('IEC-62232-panel-antenna (4)_Line2.efe','IEC-62232-panel-antenna (4)_Line2.hfe',compress=False, power=80).df
+    line2['IXUS'] = [x/100*FCCoccupational for x in IXUS2_persentage_occupation]
+    line2['Sff'] = ffline2()
+    line2['l'] = l2
+    line3 = GetField('IEC-62232-panel-antenna (4)_Line3.efe','IEC-62232-panel-antenna (4)_Line3.hfe',compress=False, power=80).df
+    line3['IXUS'] = [x/100*FCCoccupational for x in IXUS3_persentage_occupation]
+    line3['Sff'] = ffline3()
+    line3['l'] = l3
+
+    Doline(line1.rename(columns = {'X': '1D'}), line2.rename(columns = {'Y': '1D'}),line3.rename(columns = {'Z': '1D'}))
+    
+    plt.show()
 
--- main.py
+++ main.py
@@ -1,60 +1,57 @@
-from EMFFeko import GetField
+from audioop import lin2adpcm
+from cProfile import label
+from matplotlib.dates import SecondLocator
+from sympy import GoldenRatio
+from EMFFeko import GetField,GetFarField,nearSurface,ClassicalSpherical,SpacialAverageCylindricalEstimation,SpacialPeakCylindricalEstimation,AdjustedSphericalSector,SimpleSphericalSector
 from Standard import getStandard,getZone
 from matplotlib import pyplot as plt
 import pandas as pd
 import numpy as np
 from EMFIXUS import IXUSField
 import time
+import mayavi.mlab as mlab
+
+def CylindricalValidationTest():
+    SectorCoverageSbar = [5.58, 3.54, 2.49, 1.86, 1.43, 1.02, 0.639]
+    SectorCoverageS = [9.96, 5.74, 3.70, 2.56, 1.86, 1.25, 0.727]
+    f = 925
+    lamda = (3*10**8)/(f*10**6)
+    power = 80
+    L = 2.158
+    AHPBW = 84
+    y = 0
+    Gs = 17 #dBi
+    Go = 11 #dBi
+    Globe = -9      #dBi
+    Globe = -3.6    #dBi
+    phi = np.pi/12
+    Ry = [4, 6, 8, 10, 12, 15, 20]
+    R = [ry*np.cos(y*np.pi/180) for ry in Ry]
+
+    SectorAverage = SpacialAverageCylindricalEstimation(phi,R,power,AHPBW, L,Gs, y*np.pi/180,Ry)
+    SectorPeak = SpacialPeakCylindricalEstimation(phi,R,power,AHPBW, L, Gs, y=y*np.pi/180)
+
+    plt.figure()
+    plt.plot(Ry,SectorAverage,label = 'SpacialPeakCylindrical')
+    plt.plot(Ry,SectorCoverageS,label = 'Peak Cylindrical Validation line')
+    plt.plot(Ry,SectorCoverageSbar,label = 'Average Cylindrical Validation line')
+    plt.plot(Ry,SectorPeak,label = 'SpacialAverageCylindrical')
+    plt.legend()
+    plt.show()
 
 
-FCCpublic = getZone(900,'FCC')[1]
-print(FCCpublic)
-l1 = [65.8, 39.4, 25, 13.4, 10.7, 10.4, 10.1, 9.44]
-l2 = [6.22, 9.16, 13.6, 19.6, 26.3, 32.8, 37.6, 39.4, 37.6, 32.8, 26.3, 19.6, 13.6, 9.16, 6.22]
-l3 = [0.173, 3.77, 129, 105, 111, 163, 272, 338, 272, 163, 111, 105, 129, 3.77, 0.173]
 
 
 
 
-IXUS_E_l1 = [236.3, 151.5, 130.2, 90.72, 79.83, 79.23, 78.48, 76.06] 
-IXUS_S_l1 = [x**2/(2*377) for x in IXUS_E_l1]
-
-IXUS_E_l2 = [56.89, 70.7, 87.97, 106.1, 123.3, 137.8, 147.4, 151.5, 147.8, 138.5, 124, 106.6, 87.97, 70.55, 57]
-
-#IXUS_E_l2 = [76.13, 94.48, 117.7, 142, 165.1, 184.4, 197.2, 202.7, 197.8, 185.3, 165.9, 142.6, 117.7, 94.41, 76.27]
-IXUS_S_l2 = [x**2/(2*377) for x in IXUS_E_l2]
-
-IXUS_E_l3 = [18.09, 38.67, 1.932*10**8, 127.7, 163.2, 190, 228.5, 1.413*10**8, 220.8, 189.4, 164.3, 132.8, 2.005*10**7, 39.58, 18.51]
-IXUS_S_l3 = [x**2/(2*377) for x in IXUS_E_l3]
 
 
-def Doline(line1,line2, line3):
-    
-    fig, axs = plt.subplots(2,2)
-
-    axs[0, 0].plot(line1.df['X'], l1,label = 'S from Vitas')
-    axs[0, 0].plot(line1.df['X'], IXUS_S_l1,label = 'IXUS')
-    axs[0, 0].plot(line1.df['X'], line1.df['S(E)'], label = 'S(E)')
-    axs[0, 0].plot(line1.df['X'], line1.df['S(ExH)'], label = 'S(ExH)')
-    axs[0, 0].legend()
- 
-    axs[0, 1].plot(line2.df['Y'], l2,label = 'S from Vitas')
-    axs[0, 1].plot(line2.df['Y'], IXUS_S_l2,label = 'IXUS')
-    axs[0, 1].plot(line2.df['Y'], line2.df['S(E)'], label = 'S(E)')
-    axs[0, 1].plot(line2.df['Y'], line2.df['S(ExH)'], label = 'S(ExH)')
-    axs[0, 1].legend()
-
-    axs[1, 0].plot(line3.df['Z'], l3,label = 'S from Vitas')
-    axs[1, 0].plot(line3.df['Z'], IXUS_S_l3,label = 'IXUS')
-    axs[1, 0].plot(line3.df['Z'], line3.df['S(E)'], label = 'S(E)')
-    axs[1, 0].plot(line3.df['Z'], line3.df['S(ExH)'], label = 'S(ExH)')
-    axs[1, 0].legend()
- 
-line1 = GetField('IEC-62232-panel-antenna (4)_Line1.efe','IEC-62232-panel-antenna (4)_Line1.hfe',compress=False)
-line2 = GetField('IEC-62232-panel-antenna (4)_Line2.efe','IEC-62232-panel-antenna (4)_Line2.hfe',compress=False)
-line3 = GetField('IEC-62232-panel-antenna (4)_Line3.efe','IEC-62232-panel-antenna (4)_Line3.hfe',compress=False)
-Doline(line1, line2, line3)
-plt.show()
 
 
+
+
+
+
+
+CylindricalValidationTest()
 
